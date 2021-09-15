b98ea0ec6dec432bf091d0cc9a6fd794cecd6b40
isakjensen <isakludvigjensen@gmail.com>
2021-09-15 15:05:13 +0200

    Game scene rendering

--- public/js/game.js
+++ public/js/game.js
@@ -1,35 +1,71 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-let media = './media/';
 let game = document.querySelector("#game");
 let player = document.querySelector("#player");
-let buttons = document.querySelector("#buttons");
+let hud = document.querySelector("#hud");
+
+let currentScene = 0;
 
 let scenes = [
     {
+        id: 0,
+        image: './media/img/Scene_1.jpg',
+        video: null,
+        options: ['left', 'right'],
+        hp: 100,
+        next: 1,
+    },
+    {
         id: 1,
-        image: media + 'img/Scene_1.jpg',
-        options: ['l', 'r'],
-        video: media + 'video/Scene_1.mp4',
+        image: 'https://i.imgur.com/Lnhfx0x.jpg',
+        video: './media/video/Scene_1.mp4',
+        options: ['left', 'right'],
         hp: 100,
     },
 ];
 
-console.log(game.style);
-
-
 (async function() {
-    buttons.style.visibility = 'hidden';
+    RenderScene(scenes[0]);
+})();
 
-    game.style.backgroundImage = `url(${scenes[0].image}`;
+function PreRender(id)
+{
+    currentScene = id;
 
-    await delay(5000);
+    RenderScene(scenes[currentScene]);
 
-    console.log("Play video...");
+    hud.style.visibility = 'hidden';
+}
 
+async function RenderScene(scene)
+{
+    if(scene.video == null)
+    {
+        AddImage(scene.image, scene);
+    }
+    else
+    {
+        AddVideo(player, scene)
+    }
+}
+
+function DisplayHud(scene)
+{
+    console.log(scene);
+
+    for (let i = 0; i < scene.options.length; i++)
+    {
+        hud.innerHTML += `<button id="${i + 1}" onclick="PreRender(this.id)" class="bg-gray-200 border-2 border-gray-400 shadow-lg rounded-md px-3 py-2 mx-12 transform hover:scale-105">${scene.options[i]}</button> `
+    }
+
+    hud.style.visibility = 'visible';
+}
+
+async function AddVideo(player, scene)
+{
     player.innerHTML += `
     <video id="video" width="100%" height="100%">
-        <source src="${scenes[0].video}" type="video/mp4">
+        <source src="${scene.video}" type="video/mp4">
         Your browser does not support HTML5 video.
     </video>
     `;
@@ -38,17 +74,19 @@ console.log(game.style);
     let duration = 0;
 
     video.addEventListener('loadedmetadata', function () {
-        DisplayButtons(video)
+        duration = video.duration;
     });
 
-    await delay(2500)
+    video.play();
 
-    console.log(duration);
+    await delay((video.duration * 1000) + 750);
 
-    buttons.style.visibility = 'visible';
-})()
+    DisplayHud(scene);
+}
 
-async function DisplayButtons(video)
+function AddImage(image, scene)
 {
-    
+    game.style.backgroundImage = `url(${image})`;
+
+    DisplayHud(scene);
 }
